0ad8330ba3d87ca122baecdaa0f42138240654ed
Huzaifa0888 <115368837+Huzaifa0888@users.noreply.github.com>
2023-03-31 03:55:13 +0500

    link menu

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -6,10 +6,61 @@ import {
 } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Fragment, useState } from "react";
-import { Dialog, Popover, Transition } from "@headlessui/react";
+import { Dialog, Popover,Tab, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Disclosure, Menu } from "@headlessui/react";
 import { Bars3Icon, BellIcon } from "@heroicons/react/24/outline";
+
+const navigationn = {
+  categories: [
+    {
+      id: "men",
+      name: "DEMO",
+
+      sections: [
+        {
+          id: "clothing",
+          name: "Clothing",
+          items: [
+            { name: "Tops", href: "#" },
+            { name: "Pants", href: "#" },
+            { name: "Sweaters", href: "#" },
+            { name: "T-Shirts", href: "#" },
+            { name: "Jackets", href: "#" },
+            { name: "Activewear", href: "#" },
+            { name: "Browse All", href: "#" },
+          ],
+        },
+        {
+          id: "accessories",
+          name: "Accessories",
+          items: [
+            { name: "Watches", href: "#" },
+            { name: "Wallets", href: "#" },
+            { name: "Bags", href: "#" },
+            { name: "Sunglasses", href: "#" },
+            { name: "Hats", href: "#" },
+            { name: "Belts", href: "#" },
+          ],
+        },
+        {
+          id: "brands",
+          name: "Brands",
+          items: [
+            { name: "Re-Arranged", href: "#" },
+            { name: "Counterfeit", href: "#" },
+            { name: "Full Nelson", href: "#" },
+            { name: "My Way", href: "#" },
+          ],
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: "Company", href: "#" },
+    { name: "Stores", href: "#" },
+  ],
+};
 const navigation = {
   categories: [
     {
@@ -206,16 +257,205 @@ export default function Example() {
                   </div>
                   <div className="hidden sm:ml-6 sm:block">
                     <div className="flex space-x-4">
-                      <a
-                        href="/"
-                        className={classNames(
-                          false ? "bg-gray-900 text-white" : "text-black",
-                          "rounded-md px-3 py-2 text-base font-extrabold"
-                        )}
-                        aria-current="page"
-                      >
-                        DEMO
-                      </a>
+                      <div className="bg-white">
+                        {/* Mobile menu */}
+                        <Transition.Root show={open} as={Fragment}>
+                          <Dialog
+                            as="div"
+                            className="relative z-40 lg:hidden"
+                            onClose={setOpen}
+                          >
+                            <Transition.Child
+                              as={Fragment}
+                              enter="transition-opacity ease-linear duration-300"
+                              enterFrom="opacity-0"
+                              enterTo="opacity-100"
+                              leave="transition-opacity ease-linear duration-300"
+                              leaveFrom="opacity-100"
+                              leaveTo="opacity-0"
+                            >
+                              <div className="fixed inset-0 bg-black bg-opacity-25" />
+                            </Transition.Child>
+
+                            <div className="fixed inset-0 z-40 flex">
+                              <Transition.Child
+                                as={Fragment}
+                                enter="transition ease-in-out duration-300 transform"
+                                enterFrom="-translate-x-full"
+                                enterTo="translate-x-0"
+                                leave="transition ease-in-out duration-300 transform"
+                                leaveFrom="translate-x-0"
+                                leaveTo="-translate-x-full"
+                              >
+                                <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
+                                  <div className="flex px-4 pb-2 pt-5">
+                                    <button
+                                      type="button"
+                                      className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
+                                      onClick={() => setOpen(false)}
+                                    >
+                                      <span className="sr-only">
+                                        Close menu
+                                      </span>
+                                      <XMarkIcon
+                                        className="h-6 w-6"
+                                        aria-hidden="true"
+                                      />
+                                    </button>
+                                  </div>
+
+                                  {/* Links */}
+                                  <Tab.Group as="div" className="mt-2">
+                                    <div className="border-b border-gray-200">
+                                      <Tab.List className="-mb-px flex space-x-8 px-4">
+                                        {navigationn.categories.map(
+                                          (category) => (
+                                            <Tab
+                                              key={category.name}
+                                              className={({ selected }) =>
+                                                classNames(
+                                                  selected
+                                                    ? "border-indigo-600 text-indigo-600"
+                                                    : "border-transparent text-gray-900",
+                                                  "flex-1 whitespace-nowrap border-b-2 px-1 py-4 text-base font-medium"
+                                                )
+                                              }
+                                            >
+                                              {category.name}
+                                            </Tab>
+                                          )
+                                        )}
+                                      </Tab.List>
+                                    </div>
+                                    <Tab.Panels as={Fragment}>
+                                      {navigationn.categories.map(
+                                        (category) => (
+                                          <Tab.Panel
+                                            key={category.name}
+                                            className="space-y-10 px-4 pb-8 pt-10"
+                                          >
+                                            {category.sections.map(
+                                              (section) => (
+                                                <div key={section.name}>
+                                                  <p
+                                                    id={`${category.id}-${section.id}-heading-mobile`}
+                                                    className="font-medium text-gray-900"
+                                                  >
+                                                    {section.name}
+                                                  </p>
+                                                  <ul
+                                                    role="list"
+                                                    aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
+                                                    className="mt-6 flex flex-col space-y-6"
+                                                  >
+                                                    {section.items.map(
+                                                      (item) => (
+                                                        <li
+                                                          key={item.name}
+                                                          className="flow-root"
+                                                        >
+                                                          <a
+                                                            href={item.href}
+                                                            className="-m-2 block p-2 text-gray-500"
+                                                          >
+                                                            {item.name}
+                                                          </a>
+                                                        </li>
+                                                      )
+                                                    )}
+                                                  </ul>
+                                                </div>
+                                              )
+                                            )}
+                                          </Tab.Panel>
+                                        )
+                                      )}
+                                    </Tab.Panels>
+                                  </Tab.Group>
+                                </Dialog.Panel>
+                              </Transition.Child>
+                            </div>
+                          </Dialog>
+                        </Transition.Root>
+                        {navigationn.categories.map((category) => (
+                          <Popover key={category.name} className="flex">
+                            {({ open }) => (
+                              <>
+                                <div className="relative flex">
+                                  <Popover.Button
+                                    className={classNames(
+                                      open ? "text-black" : "text-black",
+                                      "rounded-md px-3 py-2 text-base font-extrabold"
+                                    )}
+                                  >
+                                    {category.name}
+                                  </Popover.Button>
+                                </div>
+
+                                <Transition
+                                  as={Fragment}
+                                  enter="transition ease-out duration-200"
+                                  enterFrom="opacity-0"
+                                  enterTo="opacity-100"
+                                  leave="transition ease-in duration-150"
+                                  leaveFrom="opacity-100"
+                                  leaveTo="opacity-0"
+                                >
+                                  <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
+                                    <div
+                                      className="absolute inset-0 top-1/2  shadow"
+                                      aria-hidden="true"
+                                    />
+
+                                    <div className="relative max-w-[800px]  bg-white">
+                                      <div className="mx-auto max-w-7xl px-8">
+                                        <div className="grid grid-cols-1 gap-x-8 gap-y-10 py-16">
+                                          <div className="row-start-1 grid grid-cols-3 gap-x-8 gap-y-10 text-sm">
+                                            {category.sections.map(
+                                              (section) => (
+                                                <div key={section.name}>
+                                                  <p
+                                                    id={`${section.name}-heading`}
+                                                    className="font-extrabold text-gray-900"
+                                                  >
+                                                    {section.name}
+                                                  </p>
+                                                  <ul
+                                                    role="list"
+                                                    aria-labelledby={`${section.name}-heading`}
+                                                    className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
+                                                  >
+                                                    {section.items.map(
+                                                      (item) => (
+                                                        <li
+                                                          key={item.name}
+                                                          className="flex"
+                                                        >
+                                                          <a
+                                                            href={item.href}
+                                                            className="font-semibold hover:text-gray-800"
+                                                          >
+                                                            {item.name}
+                                                          </a>
+                                                        </li>
+                                                      )
+                                                    )}
+                                                  </ul>
+                                                </div>
+                                              )
+                                            )}
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </Popover.Panel>
+                                </Transition>
+                              </>
+                            )}
+                          </Popover>
+                        ))}
+                     
+                      </div>
                       {navigation.categories.map((category) => (
                         <Popover key={category.name} className="flex">
                           {({ open }) => (
@@ -246,7 +486,7 @@ export default function Example() {
                                     aria-hidden="true"
                                   />
 
-                                  <div className="relative   bg-white">
+                                  <div className="relative w-full  bg-white">
                                     <div className="mx-auto max-w-7xl px-8">
                                       <div className="grid grid-cols-2  gap-x-8 gap-y-10 py-16">
                                         {
@@ -256,7 +496,7 @@ export default function Example() {
                                                 key={item.name}
                                                 className="group relative text-base  sm:text-sm"
                                               >
-                                                <div className="  overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                                                <div className="  overflow-hidden  rounded-lg bg-gray-100 group-hover:opacity-75">
                                                   <img
                                                     src={item.imageSrc}
                                                     alt={item.imageAlt}
@@ -323,6 +563,7 @@ export default function Example() {
                           )}
                         </Popover>
                       ))}
+
                       <Menu
                         as="div"
                         className="relative inline-block text-left"
@@ -347,7 +588,7 @@ export default function Example() {
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Menu.Items className="absolute right-0 left-0  z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Items className="absolute right-0 left-0  z-10 mt-4 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <div className="py-1">
                               <Menu.Item>
                                 {({ active }) => (
@@ -488,7 +729,7 @@ export default function Example() {
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
                         >
-                          <Menu.Items className="absolute right-0 left-0  z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Items className="absolute right-0 left-0  z-10 mt-4 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <div className="py-1">
                               <Menu.Item>
                                 {({ active }) => (
